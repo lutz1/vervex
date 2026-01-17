81870d1dc711dd8e714f67236c2855fb01092f82
Johnn <36783765+lutz1@users.noreply.github.com>
2026-01-17 19:30:37 +0800

    Updates

---
+++ service-worker.js
@@ -0,0 +1,3 @@
+// This file is required for Create React App's PWA support.
+// It will be automatically generated and injected during the build process.
+// You do not need to edit this file manually.
